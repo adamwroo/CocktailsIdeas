b7d6c6771dc9b5efb0130cab246c3835d930cd15
adamwroo <adamwroo@gmail.com>
2020-11-28 18:27:47 +0100

    - cocktail class added - cocktails page added (with dummy view) - cocktails api controller added (with dummy get)

---
+++ Server/CocktailsController.cs
@@ -0,0 +1,26 @@
+﻿using CocktailsIdeas.Shared;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CocktailsIdeas.Server
+{
+    [Route("cocktails")]
+    [ApiController]
+    public class CocktailsController : Controller
+    {
+        [HttpGet]
+        public async Task<ActionResult<List<Cocktail>>> GetCocktails()
+        {
+            var dummyCocktailList = new List<Cocktail>
+            {
+                new Cocktail {Id = "1", Name = "Cuba Libre"},
+                new Cocktail {Id = "2", Name = "Mojito"}
+            };
+
+            return await Task.FromResult(dummyCocktailList);
+        }
+    }
+}
---
+++ Shared/Cocktail.cs
@@ -0,0 +1,11 @@
+﻿using System.Text.Json.Serialization;
+
+namespace CocktailsIdeas.Shared
+{
+    public class Cocktail
+    {
+        [JsonPropertyName("id")]
+        public string Id { get; set; }
+        public string Name { get; set; }
+    }
+}
