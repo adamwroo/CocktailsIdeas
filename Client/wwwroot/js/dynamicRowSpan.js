0f7a70448695b13b3fef3ebecf2faad6ea0e026b
adamwroo <adamwroo@gmail.com>
2020-12-07 14:04:56 +0100

    styling with dynamic rowSpan for cocktails added (todo: initial resize)

---
+++ Client/wwwroot/js/dynamicRowSpan.js
@@ -0,0 +1,27 @@
+﻿function resizeGridItem(item) {
+    let grid = document.getElementsByClassName("cocktails-grid")[0];
+    let rowHeight = parseInt(window.getComputedStyle(grid).getPropertyValue('grid-auto-rows'));
+    let rowGap = parseInt(window.getComputedStyle(grid).getPropertyValue('grid-row-gap'));
+    let rowSpan = Math.ceil((item.querySelector('.cocktail-item-content').getBoundingClientRect().height + rowGap) / (rowHeight + rowGap));
+    item.style.gridRowEnd = "span " + rowSpan;
+}
+
+function resizeAllGridItems() {
+    let allItems = document.getElementsByClassName("cocktail-item");
+    for (x = 0; x < allItems.length; x++) {
+        resizeGridItem(allItems[x]);
+    }
+}
+
+function resizeInstance(instance) {
+    let item = instance.elements[0];
+    resizeGridItem(item);
+}
+
+window.onload = resizeAllGridItems();
+window.addEventListener("resize", resizeAllGridItems);
+
+/*allItems = document.getElementsByClassName("cocktail-item");
+for (x = 0; x < allItems.length; x++) {
+    imagesLoaded(allItems[x], resizeInstance);
+}*/
