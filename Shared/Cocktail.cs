5f107459cc4a7523f415636079aa3d5e52c0903d
adamwroo <adamwroo@gmail.com>
2020-12-08 17:59:13 +0100

    custom validator for ingredients and steps added

--- Shared/Cocktail.cs
+++ Shared/Cocktail.cs
@@ -1,4 +1,5 @@
 ﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Text.Json.Serialization;
 
 namespace CocktailsIdeas.Shared
@@ -7,8 +8,14 @@ namespace CocktailsIdeas.Shared
     {
         [JsonPropertyName("id")]
         public string id { get; set; }
+        [Required(ErrorMessage = "Please provide a cocktail's name")]
+        [MinLength(5, ErrorMessage = "The name has to have at least 5 characters")]
         public string Name { get; set; }
+        [Required]
+        [MinCollectionCount(2, ErrorMessage = "Add at least one ingredient")] // todo: this should be 1
         public List<string> Ingredients { get; set; } = new List<string>();
+        [Required]
+        [MinCollectionCount(2, ErrorMessage = "Add at least one step")] // todo: this should be 1
         public List<string> Steps { get; set; } = new List<string>();
     }
 }
---
+++ Shared/MinCollectionCount.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+
+namespace CocktailsIdeas.Shared
+{
+    public class MinCollectionCount : ValidationAttribute
+    {
+        public int Count { get; set; }
+
+        public MinCollectionCount(int count)
+        {
+            Count = count;
+        }
+
+        protected override ValidationResult? IsValid(object? value, ValidationContext validationContext)
+        {
+            int count;
+            // Automatically pass if value is null. RequiredAttribute should be used to assert a value is not null.
+            if (value == null)
+            {
+                return null;
+            }
+
+            if (value is IEnumerable<object> collection)
+            {
+                count = collection.Count();
+            }
+            else
+            {
+                throw new InvalidCastException();
+            }
+
+            if (count < Count)
+            {
+                return new ValidationResult(ErrorMessage ?? $"Number of elements less than {Count}",
+                    new[] {validationContext.MemberName});
+            }
+
+            return null;
+        }
+    }
+}
