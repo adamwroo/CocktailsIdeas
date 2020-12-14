331970728eba62d0044bdbdf703555b2f9751a39
adamwroo <adamwroo@gmail.com>
2020-12-14 17:02:25 +0100

    source added

--- Shared/Cocktail.cs
+++ Shared/Cocktail.cs
@@ -1,5 +1,6 @@
 ﻿using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
+using System.Reflection.Metadata.Ecma335;
 using System.Text.Json.Serialization;
 using CocktailsIdeas.Shared.CustomValidationAttributes;
 
@@ -10,7 +11,7 @@ namespace CocktailsIdeas.Shared
         [JsonPropertyName("id")]
         public string id { get; set; }
         [Required(ErrorMessage = "Provide a name")]
-        [MinLength(3, ErrorMessage = "Name has lass than 3 characters")]
+        [MinLength(3, ErrorMessage = "Name has less than 3 characters")]
         public string Name { get; set; }
         [Required]
         [MinNonEmptyCollectionCount(1, ErrorMessage = "Add at least one ingredient")]
@@ -18,5 +19,8 @@ namespace CocktailsIdeas.Shared
         [Required]
         [MinNonEmptyCollectionCount(1, ErrorMessage = "Add at least one step")]
         public List<string> Steps { get; set; } = new List<string>();
+
+        [Url(ErrorMessage = "Incorrect URL")]
+        public string Source { get; set; }
     }
 }
